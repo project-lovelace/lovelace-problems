16d59a279638adbd9516a56dc7b8be7b826bae66
Ali Ramadhan <ali.ramadhan@uwaterloo.ca>
2017-03-19 10:50:33 -0400

    Fixed problem 1.

--- problems/problem1.py
+++ problems/problem1.py
@@ -12,8 +12,7 @@ from .abstract_problem import AbstractProblem
 class Problem1(AbstractProblem):
 
     def generate(self):
-        vP = 6 # km/s
-        vS = 3 # km/s
+        v = 3 # [km/s]: velocity of the seismic waves
 
         r0 = np.array([79.0674612756, -27.0524778103])  # temporary
 
@@ -22,16 +21,12 @@ class Problem1(AbstractProblem):
         r2 = np.array([random.uniform(-100,100), random.uniform(-100,100)])
         r3 = np.array([random.uniform(-100,100), random.uniform(-100,100)])
 
-        tP1 = np.linalg.norm(r1-r0) / vP
-        tP2 = np.linalg.norm(r2-r0) / vP
-        tP3 = np.linalg.norm(r3-r0) / vP
+        t1 = np.linalg.norm(r1-r0) / v
+        t2 = np.linalg.norm(r2-r0) / v
+        t3 = np.linalg.norm(r3-r0) / v
 
-        tS1 = np.linalg.norm(r1-r0) / vS
-        tS2 = np.linalg.norm(r2-r0) / vS
-        tS3 = np.linalg.norm(r3-r0) / vS
-
-        problem = [str(number) for number in [r1[0], r1[1], (tS1-tP1),
-                     r2[0], r2[1], (tS2-tP2), r3[0], r3[1], (tS3-tP3)]]
+        problem = [str(number) for number in [r1[0], r1[1], t1,
+                     r2[0], r2[1], t2, r3[0], r3[1], t3]]
         solution = [str(number) for number in r0.tolist()]
 
         print("Input:", ' '.join(problem))
@@ -43,24 +38,38 @@ class Problem1(AbstractProblem):
     def solve(self, problem):
         x1 = problem[0]
         y1 = problem[1]
+        t1 = problem[2]
         x2 = problem[3]
         y2 = problem[4]
+        t2 = problem[5]
         x3 = problem[6]
         y3 = problem[7]
+        t3 = problem[8]
+
+        v = 3 # [km/s]
 
         r0 = np.array([79.0674612756, -27.0524778103])  # temporary
 
-        r1 = np.array([x1, y1])
-        r2 = np.array([x2, y2])
-        r3 = np.array([x3, y3])
+        r1 = v*t1
+        r2 = v*t2
+        r3 = v*t3
 
-        # FIXME: should not rely on solution being available!
-        r1 = np.linalg.norm(r1-r0)
-        r2 = np.linalg.norm(r2-r0)
-        r3 = np.linalg.norm(r3-r0)
+        # TODO: Find my derivation and write down the steps/logic here.
+        # Setting up the equations, we get two simultaneous linear equations for
+        # x0 and y0, namely ax + by = e and cx + dy = f where
 
-        x = ( (r1**2 - r2**2 - x1**2 + x2**2 - y1**2 + y2**2)*(y1-y3) - (r3**2 - r1**2 + x1**2 - x3**2 + y1**2 - y3**2)*(y2-y1) ) / ( 2*(x1-x3)*(y1-y2) - 2*(x1-x2)*(y1-y3) )
-        y = ( x1*r2**2 - x1*r3**2 - x2*r1**2 + x2*r3**2 + x2*x1**2 - x1*x2**2 + x3*r1**2 - x3*r2**2 - x3*x1**2 + x3*x2**2 + x1*x3**2 - x2*x3**2 + x2*y1**2 - x3*y1**2 - x1*y2**2 + x3*y2**2 + x1*y3**2 - x2*y3**2) / (2 * (x2*y1 - x3*y1 - x1*y2 + x3*y2 + x1*y3 - x2*y3))
+        a = 2*(x2-x1)
+        b = 2*(y2-y1)
+        c = 2*(x3-x1)
+        d = 2*(y3-y1)
+        e = r1**2 - x1**2 - y1**2 - r2**2 + x2**2 + y2**2
+        f = r1**2 - x1**2 - y1**2 - r3**2 + x3**2 + y3**2
+
+        # Solving ax + by = e and cx + dy = f for x,y gives
+
+        x = (b*f - d*e) / (b*c - a*d)
+        y = (c*e - a*f) / (b*c - a*d)
+        solution = [x, y]
 
         solution = [str for number in [x, y]]
 
