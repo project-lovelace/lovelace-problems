6eddd7c3d0e64a66cd4024fd873ab6246dfa7709
Ali Ramadhan <ali.ramadhan@uwaterloo.ca>
2017-04-16 17:38:31 -0400

    Solution code for tutorial 1.

---
+++ tutorials/Tutorial1.py
@@ -0,0 +1,49 @@
+# problem1.py
+
+import abc
+import math
+import random
+
+import numpy as np
+
+from .abstract_problem import AbstractProblem
+
+
+class Problem1(AbstractProblem):
+
+    def generate(self):
+        K   = random.randint(500,2500)    # [geese]
+        P_0 = random.randint(5,250)       # [geese]
+        r   = random.uniform(0.005,0.025) # [1/day]
+
+        problem  = {"K" : K, "P_0" : P_0, "r" : r }
+
+        # BASIM TODO: Can't find the solution without fully solving the problem
+        # so I called self.solve() here. Hope this is okay.
+        solution = self.solve(problem)
+
+        print("Input: ", ' '.join(problem))
+        print("Generator's solution: ", ' '.join(solution))
+
+        return problem, solution
+
+
+    def solve(self, problem):
+        K   = problem['K']   # [geese]
+        r   = problem['r']   # [geese]
+        P_0 = problem['P_0'] # [1/day]
+
+        t = 0          # [days]
+        Delta_t = 0.01 # [days]
+
+        while P < (K/2):
+            P = P + (1 - P/K)*r*P*Delta_t
+            t = t + Delta_t
+
+        solution = {'t' : math.floor(t)}
+        print("Solver's solution: ", solution)
+
+        return solution
+
+    def verify(self, answer, solution):
+        return answer['t'] == solution['t']
