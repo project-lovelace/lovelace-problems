df0208c86fb3907a5e6bdeafa85291bf981005b9
Ali Ramadhan <ali.ramadhan@uwaterloo.ca>
2017-04-22 13:16:30 -0400

    Fixed bug in tutorial 1 solution. Also fucking line endings.

--- tutorials/Tutorial1.py
+++ tutorials/Tutorial1.py
@@ -9,14 +9,15 @@ from tutorials.abstract_tutorial import AbstractTutorial
 class Tutorial1(AbstractTutorial):
 
     def generate(self):
-        K   = random.randint(500,2500)    # [geese]
-        P_0 = random.randint(5,250)       # [geese]
-        r   = random.uniform(0.005,0.025) # [1/day]
+        K   = random.randint(500, 2500)     # [geese]
+        P_0 = random.randint(5, 250)        # [geese]
+        r   = random.uniform(0.005, 0.025)  # [1/day]
 
-        problem  = {"K" : K, "P_0" : P_0, "r" : r }
+        problem = {"K": K, "P_0": P_0, "r": r}
 
-        # BASIM TODO: Can't find the solution without fully solving the problem
-        # so I called self.solve() here. Hope this is okay.
+        # TODO BASIM:
+        # Can't find the solution without fully solving the problem so I called
+        # self.solve() here. Hope this is okay.
         solution = self.solve(problem)
 
         print("Input: ", ' '.join(problem))
@@ -25,18 +26,19 @@ class Tutorial1(AbstractTutorial):
         return problem, solution
 
     def solve(self, problem):
-        K   = problem['K']   # [geese]
-        r   = problem['r']   # [geese]
-        P_0 = problem['P_0'] # [1/day]
+        K   = problem['K']    # [geese]
+        r   = problem['r']    # [geese]
+        P_0 = problem['P_0']  # [1/day]
 
-        t = 0          # [days]
-        Delta_t = 0.01 # [days]
+        P = P_0
+        t = 0           # [days]
+        Delta_t = 0.01  # [days]
 
         while P < (K/2):
             P = P + (1 - P/K)*r*P*Delta_t
             t = t + Delta_t
 
-        solution = {'t' : math.floor(t)}
+        solution = {'t': math.floor(t)}
         print("Solver's solution: ", solution)
 
         return solution
