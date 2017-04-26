92ee1321646f9d3de28727bde780260c9e75468e
Ali Ramadhan <ali.ramadhan@uwaterloo.ca>
2017-04-26 17:05:45 -0400

    Comment Lovelace's algorithm a bit more.

--- problems/Problem5.py
+++ problems/Problem5.py
@@ -1,5 +1,6 @@
 # problem5.py
 # Lovelace's algorithm
+# Haven't decided what we're testing yet.
 
 import random
 import numpy as np
@@ -9,19 +10,21 @@ from problems.abstract_problem import AbstractProblem
 
 class Problem5(AbstractProblem):
     def generate(self):
-        return {'n': random.randint(3, 10)}
+        return random.randint(3, 500)
 
     @staticmethod
     def bernoulli_lovelace(n, B=None):
         from fractions import Fraction
-        if B == None:
+        if B is None:
             B = {}
 
-        if (n < 1) or (np.mod(n, 2) == 0):
-            return 0
-        elif n in B.keys():
+        if n in B.keys():
             return B[n]
+        elif (n < 1) or (np.mod(n, 2) == 0):
+            return 0
         else:
+            # My calculations gave me a formula for B_{2n+1} so I am actually
+            # calculating B_{2m+1} = B_n where m = (n+1)/2.
             m = int((n+1)/2)
             Bn = Fraction(2*m-1, 4*m+2)
             for i in range(1, 2*m-3 + 1):
@@ -33,16 +36,14 @@ class Problem5(AbstractProblem):
                 for j in range(2, i+1 + 1):
                     denominator *= j
 
-                #print(Bn)
-                #print(numerator, denominator)
-                #print(Problem5.bernoulli_lovelace(i, B))
-                Bn = Bn - Fraction(numerator, denominator) * Problem5.bernoulli_lovelace(i, B)
+                Bi = Problem5.bernoulli_lovelace(i, B)
+                Bn = Bn - Fraction(numerator, denominator)*Bi
 
-            B[n] = Bn
+            B[n] = Bn  # Store the nth Bernoulli number in the dictionary.
             return Bn
 
     def solve(self, problem):
-        return
+        return self.bernoulli_lovelace(problem)
 
     def verify(self, proposed, actual):
         return proposed == actual
@@ -57,6 +58,7 @@ class Problem5(AbstractProblem):
 if __name__ == '__main__':
     import time
 
+    # Testing run time when creating a new dictionary B each call.
     start = time.clock()
     for i in range(1, 151):
         Problem5().bernoulli_lovelace(i)
@@ -64,6 +66,7 @@ if __name__ == '__main__':
     end = time.clock()
     print(end - start)
 
+    # Testing run time with the dictionary being shared between calls.
     start = time.clock()
     B = {}
     for i in range(1, 151):
