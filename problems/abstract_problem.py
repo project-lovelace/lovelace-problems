6bda863c2c5b718953af6cdd7669a1538b2c44de
Ali Ramadhan <ali.ramadhan@uwaterloo.ca>
2017-05-15 11:14:15 -0400

    Changed up the AbstractProblem metaclass to clarify the roles of each method in preparation for switching to TestCase objects.

--- problems/abstract_problem.py
+++ problems/abstract_problem.py
@@ -6,17 +6,36 @@ import abc
 class AbstractProblem(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
-    def generate(self):
-        """Create a problem and return it with the solution."""
+    def generate_input(self):
+        """Create a set of inputs for a problem and return them as a TestCase
+        object. If the solution is known at input creation time, it should be
+        included in the TestCase."""
 
     @abc.abstractmethod
-    def solve(self, problem):
-        """Solve a given problem and return its solution."""
+    def solve_problem(self, test_case):
+        """Solve a problem given a TestCase object with an input and update the
+        TestCase object with the solution."""
 
     @abc.abstractmethod
-    def verify(self, answer, solution):
-        """Given an answer to the problem, compare it to the solution and
-        return True if correct, else False."""
+    def test_our_solution(self):
+        """Just a method used to test that our code all works (no errors) and
+        that our solution is correct (which will probably involve some solution
+        checking by humans). It should test the generate_input and solve_problem
+        methods extensively. It will probably just be run occasionally to ensure
+        our code still works in between updates.
+        """
+
+    @abc.abstractmethod
+    def verify_user_solution(self, test_case):
+        """Given a TestCase object containing the user's input and output
+        (solution), verify that they have solved the problem correctly by
+        comparing the test case our own solution. Return True if correct, else
+        False."""
+
+    @abc.abstractmethod
+    def generate_test_cases(self):
+        """Generates a bunch of TestCase objects used to check the user's
+        submitted code."""
 
     # @property
     # @abc.abstractmethod
