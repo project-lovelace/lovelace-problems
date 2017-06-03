19badea0c5131dc59a5279c76f47013f217402c9
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2017-06-02 21:41:59 -0400

    More pytest dots!

--- problems/tests/test_problem1.py
+++ problems/tests/test_problem1.py
@@ -3,14 +3,27 @@ import pytest
 from .. import problem1 as problem
 
 
-def test_all_problem1_test_cases():
+def test_general_case():
     test_case_type_enum = problem.TEST_CASE_TYPE_ENUM
+    tc = problem.generate_input(test_case_type_enum.GENERAL)
+    problem.solve_test_case(tc)
+    assert problem.verify_user_solution(tc.input_str(), tc.output_str())
 
-    test_cases = []
-    for test_type in test_case_type_enum:
-        for _ in range(test_type.multiplicity):
-            test_cases.append(problem.generate_input(test_type))
 
-    for tc in test_cases:
-        problem.solve_test_case(tc)
-        assert problem.verify_user_solution(tc.input_str(), tc.output_str())
+def test_zero_case():
+    test_case_type_enum = problem.TEST_CASE_TYPE_ENUM
+    tc = problem.generate_input(test_case_type_enum.ZERO_CASE)
+    problem.solve_test_case(tc)
+    assert problem.verify_user_solution(tc.input_str(), tc.output_str())
+
+
+def test_equidistant_case():
+    test_case_type_enum = problem.TEST_CASE_TYPE_ENUM
+    tc = problem.generate_input(test_case_type_enum.EQUIDISTANT)
+    problem.solve_test_case(tc)
+    assert problem.verify_user_solution(tc.input_str(), tc.output_str())
+
+
+def test_raises_exception_on_not_enum():
+    with pytest.raises(TypeError):
+        problem.generate_input(6969)
