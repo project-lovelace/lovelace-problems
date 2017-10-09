9baf78b7b5c98bedf0b3391290824b9757fb42c6
Ali Ramadhan <ali.ramadhan@uwaterloo.ca>
2017-10-08 21:34:21 -0400

    RNA Transcription: problem code refactored.

--- problems/dna_transcription.py
+++ problems/dna_transcription.py
@@ -1,79 +1,103 @@
-# problem3.py
-# Polymerase chain reaction
+# ProblemI4.py
+# RNA Transcription
 
-# Featuring the EcoRII restriction enzyme:
-#
-# Before:         After:
-#                      insertion
-#                        vvvvv
-# 5' NNCCWGGNN    5'---NN     CCWGGNN---3'
-# 3' NNGGWCCNN    3'---NNGGWCC     NN---5'
-
-import logging
 import random
 
-from problems.abstract_problem import AbstractProblem
+from problems.test_case import TestCase, TestCaseTypeEnum
 
-log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+from os import path
+import logging.config
+import logging
+
+logging_config_path = path.join(path.dirname(path.abspath(__file__)), '..', 'logging.ini')
+logging.config.fileConfig(logging_config_path)
+logger = logging.getLogger(__name__)
 
 
-class Problem3(AbstractProblem):
-    def generate(self):
-        dna_length = random.randint(5, 10)
-        extra_sites = random.randint(1, 2)
-        strand_length = random.randint(3, 10)
+class TestCaseI4Type(TestCaseTypeEnum):
+    RANDOM = ('Randomly generated DNA sequence', '', 5)
 
-        sequence = self.generate_dna_sequence(dna_length)
-        sequence = self.randomly_insert_extra_sites(sequence, extra_sites)
-        strand = self.generate_dna_sequence(strand_length)
 
-        problem = {'sequence': sequence, 'strand': strand}
-        return problem, None
+class TestCaseI4(TestCase):
+    def input_str(self) -> str:
+        return str(self.input['dna_str'])
 
-    @staticmethod
-    def generate_dna_sequence(length):
-        return ''.join(random.choice('ATGC') for _ in range(length))
+    def output_str(self) -> str:
+        return self.output['rna_str']
 
-    @staticmethod
-    def randomly_insert_extra_sites(sequence, n):
-        # Insert the string "CCWGG" (W = A or T) at n random locations of the
-        # string sequence.
-        # TODO: Does this run in O(n)?
-        length = len(sequence)
-        for _ in range(length):
-            site = 'CC' + random.choice('AT') + 'GG'
-            pos = random.randint(0, length-1)
-            sequence = sequence[:pos] + site + sequence[pos:]
-        return sequence
 
-    @staticmethod
-    def dna_complement(seq):
-        base_pairs = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
-        complement = [base_pairs[base] for base in list(seq)]
-        return ''.join(complement)
+TEST_CASE_TYPE_ENUM = TestCaseI4Type
+TEST_CASE_CLASS = TestCaseI4
 
-    def solve(self, problem):
-        sequence = problem['sequence']
-        strand = problem['strand']
+PHYSICAL_CONSTANTS = {}
+TESTING_CONSTANTS = {}
 
-        # TODO: Will not work if strand contains 'CCTGG'.
-        sequence = sequence.replace('CCAGG', strand + 'CCAGG')
-        sequence = sequence.replace('CCTGG', strand + 'CCTGG')
-        complement = self.dna_complement(sequence)
 
-        solution = {'complement': complement}
-        return solution
+def generate_dna_sequence(length):
+    return ''.join(random.choice('ATGC') for _ in range(length))
 
-    def verify(self, proposed, actual):
-        return proposed == actual
 
-    def test(self):
-        problem = self.generate()
-        print(problem)
+def randomly_insert_extra_sites(sequence, n):
+    # Insert the string "CCWGG" (W = A or T) at n random locations of the
+    # string sequence.
+    # TODO: Does this run in O(n)?
+    length = len(sequence)
+    for _ in range(length):
+        site = 'CC' + random.choice('AT') + 'GG'
+        pos = random.randint(0, length-1)
+        sequence = sequence[:pos] + site + sequence[pos:]
+    return sequence
 
-        solution = self.solve(problem)
-        print(solution)
 
-if __name__ == '__main__':
-    Problem3().test()
+def dna_complement(seq):
+    base_pairs = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+    complement = [base_pairs[base] for base in list(seq)]
+    return ''.join(complement)
+
+
+def generate_input(test_type: TestCaseI4Type) -> TestCaseI4:
+    test_case = TestCaseI4(test_type)
+
+    if test_type == TestCaseI4Type.RANDOM:
+        dna_length = random.randint(5, 100)
+        dna_str = generate_dna_sequence(dna_length)
+
+    test_case.input['dna_str'] = dna_str
+    return test_case
+
+
+def solve_test_case(test_case: TestCaseI4) -> None:
+    dna_str = test_case.input['dna_str']
+    test_case.output['rna_str'] = dna_complement(dna_str)
+    return
+
+
+def verify_user_solution(user_input_str: str, user_output_str: str) -> bool:
+    logger.info("Verifying user solution...")
+    logger.debug("User input string: %s", user_input_str)
+    logger.debug("User output string: %s", user_output_str)
+
+    # Build TestCase object out of user's input string.
+    tmp_test_case = TestCaseI4(TestCaseI4Type.UNKNOWN)
+
+    dna_str = user_input_str
+    tmp_test_case.input = {'dna_str': dna_str}
+
+    # Solve the problem with this TestCase so we have our own solution, and extract the solution.
+    solve_test_case(tmp_test_case)
+    rna_str = tmp_test_case.output['rna_str']
+
+    # Extract user solution.
+    user_rna_str = user_output_str
+
+    logger.debug("User solution:")
+    logger.debug("rna_str = %s", user_rna_str)
+    logger.debug("Engine solution:")
+    logger.debug("rna_str = %s", rna_str)
+
+    if rna_str == user_rna_str:
+        logger.info("User solution correct.")
+        return True
+    else:
+        logger.info("User solution incorrect.")
+        return False
