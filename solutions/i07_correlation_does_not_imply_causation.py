9d1e74d96b81d12552c03f2465abb39e9130452c
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-10-27 16:54:24 -0400

    Refactored 7. Correlation != causation.

--- solutions/i07_correlation_does_not_imply_causation.py
+++ solutions/i07_correlation_does_not_imply_causation.py
@@ -2,36 +2,25 @@ import csv
 
 import numpy as np
 
-csv_filename = input()
+def solution(x, y):
+	N = len(x)
+	x = np.array(x)
+	y = np.array(y)
+	
+	x_bar = x.mean()
+	y_bar = y.mean()
 
-x = []
-y = []
+	cov_XY = 0
+	sigma_X = 0
+	sigma_Y = 0
 
-with open(csv_filename) as csvfile:
-    xy_reader = csv.reader(csvfile, delimiter=',')
-    for row in xy_reader:
-        x.append(float(row[0]))
-        y.append(float(row[1]))
+	for i in range(N):
+		cov_XY = cov_XY + (x[i] - x_bar)*(y[i] - y_bar)
+		sigma_X = sigma_X + (x[i] - x_bar)**2
+		sigma_Y = sigma_Y + (y[i] - y_bar)**2
 
-N = len(x)
-x = np.array(x)
-y = np.array(y)
+	sigma_X = np.sqrt(sigma_X)
+	sigma_Y = np.sqrt(sigma_Y)
 
-x_bar = x.mean()
-y_bar = y.mean()
-
-cov_XY = 0
-sigma_X = 0
-sigma_Y = 0
-
-for i in range(N):
-    cov_XY = cov_XY + (x[i] - x_bar)*(y[i] - y_bar)
-    sigma_X = sigma_X + (x[i] - x_bar)**2
-    sigma_Y = sigma_Y + (y[i] - y_bar)**2
-
-sigma_X = np.sqrt(sigma_X)
-sigma_Y = np.sqrt(sigma_Y)
-
-r = cov_XY / (sigma_X * sigma_Y)
-
-print('{}'.format(r))
+	r = cov_XY / (sigma_X * sigma_Y)
+	return r
