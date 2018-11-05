e1b4024976cc1296f088acfc17531c3dd855b1a7
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-11-04 21:17:25 -0500

    Updated solution to problem 9 to fit new descr.

--- solutions/i09_el_nino_intensities.py
+++ solutions/i09_el_nino_intensities.py
@@ -1,28 +1,44 @@
 import csv
 
-def solution():
-    years = []
-    mei = []
+years = []
+mei = []
 
-    with open('mei.ext_index.txt') as csvfile:
-        reader = csv.reader(csvfile, delimiter='\t')
-        next(reader, None)  # skip the header line
-        for row in reader:
-            years.append(int(row[0]))
-            mei.append([float(i) for i in row[1:]])
+def mei_to_intensity(mei_val):
+    mei_val = abs(mei_val)
+    if mei_val < 0.5:
+        return "none"
+    elif 0.5 <= mei_val < 1.0:
+        return "weak"
+    elif 1.0 <= mei_val < 1.5:
+        return "moderate"
+    elif 1.5 <= mei_val < 2.0:
+        return "strong"
+    elif 2.0 <= mei_val:
+        return "very strong"
 
-    classification_str = ''
-    for i in range(len(years) - 1):
-        season = str(years[i])[:4] + '-' + str(years[i + 1])[-2:]
-        season_mei = mei[i] + mei[i + 1]
-        for j in range(len(season_mei) - 5):
-            if all(idx >= 0.5 for idx in season_mei[j:j + 5]):
-                max_mei = max(season_mei)
-                classification_str = classification_str + '{}: El Nino (MEI={})'.format(season, max_mei) + '\n'
-                break
-            elif all(idx <= -0.5 for idx in season_mei[j:j + 5]):
-                min_mei = min(season_mei)
-                classification_str = classification_str + '{}: La Nina (MEI={})'.format(season, min_mei) + '\n'
-                break
-                
-    return classification_str
+with open('mei.ext_index.txt') as csvfile:
+    reader = csv.reader(csvfile, delimiter='\t')
+    next(reader, None)  # skip the header line
+    for row in reader:
+        years.append(int(row[0]))
+        mei.append([float(i) for i in row[1:]])
+
+mei_dict = {}
+for i in range(len(years) - 1):
+    season = str(years[i])[:4] + '-' + str(years[i + 1])[-2:]
+    season_mei = mei[i] + mei[i+1]
+    for j in range(len(season_mei) - 5):
+        if all(idx >= 0.5 for idx in season_mei[j:j + 5]):
+            max_mei = max(season_mei)
+            
+            mei_dict[season] = ("El Nino", mei_to_intensity(max_mei), max_mei)
+            break
+        elif all(idx <= -0.5 for idx in season_mei[j:j + 5]):
+            min_mei = min(season_mei)
+            mei_dict[season] = ("La Nina", mei_to_intensity(min_mei), min_mei)
+            break
+        else:
+            mei_dict[season] = ("Neither", "none")
+			
+def enso_classification(season):
+    return mei_dict[season]
