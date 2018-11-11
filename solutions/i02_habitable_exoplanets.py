61bf1f7dc2896fa4cce2e9914db686c3d8970ccf
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-11-10 22:31:20 -0500

    Updated problems 1-3.

--- solutions/i02_habitable_exoplanets.py
+++ solutions/i02_habitable_exoplanets.py
@@ -1,10 +1,12 @@
 import math
 
-
-def solution(L_star, r):
-    if r < math.sqrt(L_star / 1.1):
+def habitable_exoplanet(L, r):
+	inner_radius = math.sqrt(L / 1.1)   # [AU]
+	outer_radius = math.sqrt(L / 0.53)  # [AU]
+    
+    if r < inner_radius:
         return 'too hot'
-    elif r > math.sqrt(L_star / 0.53):
+    elif r > :
         return 'too cold'
     else:
         return 'just right'
