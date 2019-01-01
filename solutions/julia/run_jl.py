1d01e3fbc6a795eb5fba79bcfa4b750e05660a19
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2019-01-01 16:46:41 -0500

    Prototype julia code runner for the engine.

--- solutions/julia/run_jl.py
+++ solutions/julia/run_jl.py
@@ -1,11 +1,24 @@
 import julia
 
 j = julia.Julia()
-j.include("habitable-exoplanet.jl")
+j.include("test-function.jl")
 
-user_input = (1.45, 2.28)
+user_input = (3, 7, 12)
 
-user_output = j.habitable_exoplanet(*user_input)
+j.timed_function_call(j.test_function, user_input)  # Call function to pre/compile.
+submission_output = j.timed_function_call(j.test_function, user_input)
 
-print(user_input)
-print(user_output)
+user_output = submission_output[0]
+runtime = submission_output[1]
+bytes_allocated = submission_output[2]
+
+# print(submission_output[0])
+# print(submission_output[1])
+#
+# user_output = submission_output[0]
+# runtime = submission_output[1]
+#
+print("User input: {:}".format(user_input))
+print("User output: {:}".format(user_output))
+print("Runtime: {:} seconds".format(runtime))
+print("Memory allocated: {:} bytes".format(bytes_allocated))
