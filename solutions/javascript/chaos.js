2c9c8b21a1ebd98f48c6b602b153a3fe20d78115
basimr <basim.ramadhan@gmail.com>
2019-01-01 12:50:38 -0500

    Added a few Javascript solutions

---
+++ solutions/javascript/chaos.js
@@ -0,0 +1,5 @@
+def logistic_map(r):
+    x = [0.5]
+    for _ in range(50):
+        x.append(r * x[-1] * (1 - x[-1]))
+    return x
---
+++ solutions/javascript/dna-transcription.js
@@ -0,0 +1,3 @@
+function rna(dna) {
+  return dna.replace("T", "U");
+}
