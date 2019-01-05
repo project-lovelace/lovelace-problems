08309316e35cf9675c480e1e5b26469e41c9da45
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-01-05 11:05:35 -0500

    Using factorial instead of binom for exact precision.

--- solutions/python/ada-lovelaces-note-g.py
+++ solutions/python/ada-lovelaces-note-g.py
@@ -2,11 +2,11 @@ from fractions import Fraction
 from scipy.special import binom
 
 def bernoulli(n):
-    B = (n+1) * [Fraction(0, 1)]
+    B = (n+2) * [Fraction(0, 1)]
     B[0] = Fraction(1, 1)
     B[1] = Fraction(-1, 2)
 
     for i in range(2, n+1):
-        B[i] = -sum([Fraction(int(binom(i, k)), (i+1-k)) * B[k]  for k in range(0, i)])
+        B[i] = -sum([Fraction(int(factorial(i)/(factorial(k)*factorial(i - k))), (i+1-k)) * B[k]  for k in range(0, i)])
 
     return B[n].numerator, B[n].denominator
