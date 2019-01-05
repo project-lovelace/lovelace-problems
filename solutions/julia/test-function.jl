15179193fb7c89f54711ebd68cd704bfe970744c
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-01-04 19:41:12 -0500

    Julia stuff moved to engine.

--- solutions/julia/test-function.jl
+++
@@ -1,7 +0,0 @@
-function timed_function_call(f, input)
-    @timed f(input...)
-end
-
-function test_function(a, b, c)
-    return (a, string(b), string(a)*string(b)*string(c), [a^2, b^2, c^3], (a^3, b^3, c^3))
-end
