40888d5513122b041559c8b5017b488c7a4c84e2
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-07-16 22:37:54 -0400

    Document physical constants in speed of light

--- problems/speed_of_light.py
+++ problems/speed_of_light.py
@@ -14,8 +14,30 @@ OUTPUT_VARS = ['time']
 STATIC_RESOURCES = []
 
 PHYSICAL_CONSTANTS = {
-    'c': 299792458  # speed of light [m/s]
+    # Source: https://en.wikipedia.org/wiki/Speed_of_light
+    'c': 299792458,  # speed of light [m/s]
+
+    # All distances in meters.
+
+    # Time-averaged distance between the Earth and lunar surfaces. The average distance between the Earth and Moon is
+    # 384,400 km but the radius of the Earth is 6,371 km and the radius of the Moon is 1,737 km, so in this case light
+    # actually travels 384,400 - 6,371, - 1,737 = 376,292 km
+    # Source: https://en.wikipedia.org/wiki/Lunar_distance_(astronomy)
+    'd_Earth_Moon': 376292e3,  # 376,292 km
+
+    # 1 Astronomical unit (almost equal to the average of Earth's aphelion and perihelion).
+    # Source: https://en.wikipedia.org/wiki/Astronomical_unit
+    'd_Sun_Earth': 149597870700,  # ~150 million kilometres
+
+    # Maximum distance between the Earth and Mars.
+    # Source: https://www.space.com/14729-spacekids-distance-earth-mars.html
+    'd_Earth_Mars': 401e9,  # ~401 million km
+
+    # Maximum distance between Earth and Jupiter.
+    # Source: https://www.universetoday.com/14514/how-far-is-jupiter-from-earth/
+    'd_Earth_Jupiter': 928e9,  # ~928 million km
 }
+
 ATOL = {}
 RTOL = {
     'time': 1e-5
@@ -45,19 +67,20 @@ def generate_test_case(test_type: TestCaseType) -> ProblemTestCase:
     test_case = ProblemTestCase(test_type)
 
     if test_type is TestCaseType.EARTH_TO_MOON:
-        distance = 385000e3  # 385,000 km
+        distance = PHYSICAL_CONSTANTS['d_Earth_Moon']
 
     elif test_type is TestCaseType.SUN_TO_EARTH:
-        distance = 1.4959802296e11  # https://www.wolframalpha.com/input/?i=mean+sun+to+earth+distance
+        distance = PHYSICAL_CONSTANTS['d_Sun_Earth']
 
     elif test_type is TestCaseType.MAX_EARTH_TO_MARS:
-        distance = 401e9  # 401 million km
+        distance = PHYSICAL_CONSTANTS['d_Earth_Mars']
 
     elif test_type is TestCaseType.MAX_EARTH_TO_JUPITER:
-        distance = 968e9  # 968 million km
+        distance = PHYSICAL_CONSTANTS['d_Earth_Jupiter']
 
     elif test_type is TestCaseType.RANDOM:
-        distance = uniform(299792458, 60*299792458)
+        c = PHYSICAL_CONSTANTS['c']
+        distance = uniform(c, 60*c)
 
     test_case.input['distance'] = distance
     return test_case
