223b56a696d21ddc3b5fb96697843ff971c36f03
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-08-04 07:34:28 -0400

    No sqrt(n < 0) and properly check n = 0

--- problems/babylonian_square_roots.py
+++ problems/babylonian_square_roots.py
@@ -1,4 +1,5 @@
 import logging
+from math import isclose
 from typing import Tuple
 
 from numpy.random import uniform, randint
@@ -23,7 +24,6 @@ RTOL = {
 
 class TestCaseType(TestCaseTypeEnum):
     ZERO = ("zero", 1)
-    NEGATIVE = ("negative number", 1)
     SMALL = ("1 < n < 10", 2)
     LARGE = ("10 < n < 1,000,000", 2)
     SQUARE = ("square number", 1)
@@ -46,9 +46,6 @@ def generate_test_case(test_type: TestCaseType) -> ProblemTestCase:
     if test_type is TestCaseType.ZERO:
         n = 0
 
-    elif test_type is TestCaseType.NEGATIVE:
-        n = uniform(-10, -0.01)
-
     elif test_type is TestCaseType.SMALL:
         n = uniform(1, 10)
 
@@ -66,5 +63,10 @@ def generate_test_case(test_type: TestCaseType) -> ProblemTestCase:
 
 def verify_user_solution(correct_test_case: TestCase, user_input: tuple, user_output: tuple) -> Tuple[bool, str]:
     user_test_case = ProblemTestCase(None, INPUT_VARS, user_input, OUTPUT_VARS, user_output)
+    
+    # Special case for n = 0 as we still want to use rtol=1e-10 on all answers but atol=1e-10 for n =0.
+    if correct_test_case.input['n'] == 0:
+        return isclose(user_test_case.output['sqrt_n'], 0, atol=RTOL['sqrt_n']), correct_test_case.output_str()
+
     passed, correct_test_case = test_case_solution_correct(correct_test_case, user_test_case, ATOL, RTOL)
     return passed, correct_test_case.output_str()
